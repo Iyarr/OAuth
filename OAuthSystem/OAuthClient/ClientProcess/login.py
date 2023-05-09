86cd486e12673841dfe9ef50d683c16f57d8c141
Iyarr <8.24ridiculous@gmail.com>
2023-05-09 23:05:20 +0900

    セッションアプリ作成

--- OAuthSystem/OAuthClient/ClientProcess/login.py
+++ OAuthSystem/OAuthClient/ClientProcess/login.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse,HttpRequest
 import oauthlib
 
 def SessionIDCheck(request,UserName):
-    if request.COOKIES.get('sessionID')  == request.session['id'] and request.COOKIES.get('UserName')  == request.session['UserName']:
+    if request.COOKIES.get('sessionID')  == request.session['id'] and UserName  == request.session['UserName']:
         return True
     else:
         
--- OAuthSystem/OAuthClient/views.py
+++ OAuthSystem/OAuthClient/views.py
@@ -9,9 +9,9 @@ def index(request):
     return render(request,'index.html')
 
 def logIn(repuest,UserName):
-    return HttpResponse("successful! Welcome " + repuest.session['UserName']
+    return HttpResponse("successful! Welcome " + UserName
         if SessionIDCheck(repuest,UserName) 
-        else "Permission Denied" 
+        else "Permission Denied"
     ) 
 
 def logOn(repuest,UserName):
@@ -21,14 +21,12 @@ def logOn(repuest,UserName):
 
     response = HttpResponse("Cookie is set")
     response.set_cookie('sessionID', sessionID )
-    response.set_cookie('UserName', UserName )
 
     return response
 
 def logOut(repuest,UserName):
     response = HttpResponse("Cookie is deleted")
     response.delete_cookie('sessionID')
-    response.delete_cookie('UserName')
 
     return response
 
