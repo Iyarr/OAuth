fed7d2e1fd9cbac84585bed6a97b0b52714553b4
Iyarr <8.24ridiculous@gmail.com>
2023-05-12 11:15:32 +0900

    プロジェクト名変更

--- Security_Camp/Security_Camp/asgi.py
+++ Security_Camp/Security_Camp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for OAuthSystem project.
+ASGI config for Security_Camp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OAuthSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Security_Camp.settings')
 
 application = get_asgi_application()
--- Security_Camp/manage.py
+++ Security_Camp/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OAuthSystem.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Security_Camp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
